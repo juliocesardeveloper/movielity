f085c7b09730fdc5f4e878d69b8dc87e8153c5f2
Julio César Arroyave Herrera <52511013+juliocesardeveloper@users.noreply.github.com>
2021-05-05 16:25:50 -0500

    Created types and reducers

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+import { types } from "../types/types";
+
+export const authReducer = ( state = {}, action ) => {
+  switch ( action.type ) {
+    case types:
+      
+      break;
+  
+    default:
+      break;
+  }
+}
